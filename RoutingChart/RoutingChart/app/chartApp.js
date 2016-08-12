47b9b0879a379d08b5b6107b616922f3d8dbdd4a
ravi <ravi@7VRQ85J>
2016-08-12 14:44:09 +0200

    Implemented model

--- RoutingChart/RoutingChart/app/chartApp.js
+++ RoutingChart/RoutingChart/app/chartApp.js
@@ -62,6 +62,7 @@ chartApp.controller('pqroutecontroller',
         $scope.calculateModel = function () {
             var parameters = $scope.parameters;
 
+
             let hl = parameters.relief;
             let ase = parameters.sjoprosent;
             let qn = parameters.normal_avlop;
@@ -75,7 +76,8 @@ chartApp.controller('pqroutecontroller',
                     calcualateT(_k1, qn).then(function (_t) {
                         $scope.modelParam.t = _t;
 
-                        calculateQSim(parameters);
+                        //calculateQSim(parameters, _k1, _k2, _t);
+                        calculateQSim(parameters, 0.094, 0.025, 14.4);
                     });
                 });
             });
@@ -109,17 +111,54 @@ chartApp.controller('pqroutecontroller',
 
 
 
-        function calculateQSim(parameters) {
-            let sm = parameters.soil_water_moisture;
-            let fc = parameters.field_capacity;
-            let beta = parameters.beta;
+        function calculateQSim(parameters, k1, k2, t) {
+            //let sm = parameters.soil_water_moisture;
+            //let fc = parameters.field_capacity;
+            //let beta = parameters.beta;
+            let a_m2 = 50000000;
 
-            let duz = '';
-            for (var i = 0; i < $scope.tmpPrecipitations.length; i++) {
+            let sm = 100;
+            let fc = 100;
+            let beta = 2;
+            let perc = 0.0025;
+            let uz1 = 100;
+            let pArr = [2.8, 2.8, 2.8, 2.8, 2.8, 2.8, 3.9, 3.9];
+            let klz = 0.00042;
 
-                caculateDuz(sm, fc, beta, p).then(function (_duz) {
-                    duz = _duz;
-                });
+            let smArr = [];
+            let duzArr = [];
+            let uzArr = [];
+            let lzArr = [];
+            let q1Arr = [];
+            let q2Arr = [];
+            let qSimArr = [];
+
+            smArr[0] = 100;
+            duzArr[0] = 2.8;
+            uzArr[0] = 3.15;
+            lzArr[0] = 6;
+            q1Arr[0] = 0.5;
+            q2Arr[0] = 0.5;
+
+            for (var i = 1; i < pArr.length; i++) {
+                smArr[i] = sm + pArr[i - 1] - duzArr[i - 1];
+
+                duzArr[i] = $window.Math.pow((smArr[i] / fc), beta) * pArr[i];
+                uzArr[i] = uzArr[i - 1] + duzArr[i] - perc - q1Arr[i - 1];
+
+                if (uzArr[i] <= t) { // assumes t is threshold in mm (uz1)
+                    q1Arr[i] = k2 * uzArr[i];
+                } else {
+                    q1Arr[i] = k1 * (uzArr[i] - t) + k2 * t;
+                }
+
+
+                lzArr[i] = lzArr[i - 1] + perc - q2Arr[i - 1];
+                q2Arr[i] = klz * lzArr[i];
+
+                qSimArr[i] = q1Arr[i] + q2Arr[i];
+                var qsim = (a_m2 * (qSimArr[i] / 1000)) / 3600;
+                console.log('Qsim: ' + qsim);
             }
         };
 
@@ -152,23 +191,24 @@ chartApp.controller('pqroutecontroller',
 
 
 
-        $scope.drawChart = function () {
-            $scope.tmpPrecipitations = [
-                    '0.1', '0', '0.8', '0.9', '20.9', '17.9', '2.7', '1.4', '1.4', '0', '0', '0', '0', '0.6', '0.2',
-                    '0.2', '0.2', '0.4', '0', '0', '0', '0', '0', '0', '0'
-            ],
-              $scope.tmpTemperature = [
-                    '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
-                    '0', '0', '0', '0', '0'
-              ],
-                $scope.tmpQObs = [
-                    '0.08', '0.08', '0.8', '0.09', '3.12', '8.25', '8.59', '6.17', '5.33', '5.33', '3.57', '3.57',
-                    '2.57', '2.17', '1.87', '1.66', '1.48', '1.34', '1.24', '1.14', '1.04', '0.97', '0.87', '0.77',
-                    '0.67'
-                ],
-            $scope.tmpTimespans = [
-                1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26
-            ];
+        $scope.drawChart = function (qObs, qSim) {
+            $scope.tmpPrecipitations = qObs;
+            //$scope.tmpPrecipitations = [
+            //        '0.1', '0', '0.8', '0.9', '20.9', '17.9', '2.7', '1.4', '1.4', '0', '0', '0', '0', '0.6', '0.2',
+            //        '0.2', '0.2', '0.4', '0', '0', '0', '0', '0', '0', '0'
+            //],
+            //  $scope.tmpTemperature = [
+            //        '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+            //        '0', '0', '0', '0', '0'
+            //  ],
+            //    $scope.tmpQObs = [
+            //        '0.08', '0.08', '0.8', '0.09', '3.12', '8.25', '8.59', '6.17', '5.33', '5.33', '3.57', '3.57',
+            //        '2.57', '2.17', '1.87', '1.66', '1.48', '1.34', '1.24', '1.14', '1.04', '0.97', '0.87', '0.77',
+            //        '0.67'
+            //    ],
+            //$scope.tmpTimespans = [
+            //    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26
+            //];
 
             $scope.labels = $scope.tmpTimespans;
             $scope.data = [
